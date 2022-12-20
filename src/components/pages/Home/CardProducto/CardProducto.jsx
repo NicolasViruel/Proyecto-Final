b9802f770fd3aff229db994a8ef9e084674837cc
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2022-12-20 20:33:34 -0300

    se arreglo los botones en las cards del Home

--- src/components/pages/Home/CardProducto/CardProducto.jsx
+++ src/components/pages/Home/CardProducto/CardProducto.jsx
@@ -12,7 +12,7 @@ const CardProducto = ({ img, name, text, graduation, price, id, category }) => {
     <Container>
       <Row>
         <Col>
-          <Card className="my-4 h-100 card-test" style={{ position: 'relative' }}>
+          <Card className=" h-100 card-test" style={{ position: 'relative' }}>
             <Card.Img variant="top" src={img} className="imagen-tarjeta" />
             <Card.Body>
               <div className="d-flex align-items-center justify-content-between mb-2">
@@ -25,16 +25,17 @@ const CardProducto = ({ img, name, text, graduation, price, id, category }) => {
               <Card.Text>
                 Graduacion: {graduation}
               </Card.Text>
-              <Card.Text className='textCard'>
+              <Card.Text className=''>
                 Precio: ${price}
               </Card.Text>
-              <div style={{ position: 'absolute', bottom: 0, width: "100%" }} className="mb-3">
-                <Button type="submit" variant="info"> Add to 🛒</Button>
-                <Button variant='danger' onClick={() => navigate(`/favoritos`)}>Add to ❤</Button>
-                {/* <Button variant="secondary" onClick={() => navigate(`/products/${id}`)}>
-                  Details
-                </Button> */}
-              </div>
+              <div className="d-grid gap-2">
+                  <Button type="submit" variant="info" onClick={() => navigate(`/buy`)}>
+                    Add to 🛒
+                  </Button>
+                  <Button variant='danger' onClick={() => navigate(`/favoritos`)}>
+                    Add to ❤
+                  </Button>
+                </div>
             </Card.Body>
           </Card>
         </Col>
