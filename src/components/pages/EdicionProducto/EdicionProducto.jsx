437a444e6830e3e92b51709e52d9c116dd40e562
josetrivino89 <josetrivino89@gmail.com>
2022-12-29 17:43:46 -0300

    se termino las validaciones en la creacion y edicion de productos

--- src/components/pages/EdicionProducto/EdicionProducto.jsx
+++ src/components/pages/EdicionProducto/EdicionProducto.jsx
@@ -129,63 +129,7 @@ const EdicionProducto = (props) => {
     props.funcNav(true);
   }, []);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   //validador de campos
-  //   if (
-  //     !validateProductName(productoNameRef.current.value) ||
-  //     !validateDetalleProducto(productoEditarRef.current.value) ||
-  //     !validatePrice(productoPriceRef.current.value) ||
-  //     !validateUrl(productoUrlRef.current.value) ||
-  //     !validateCategory(productoEditar.Category) ||
-  //     validatePorcentaje(productoPorcentajeRef.current.value)
-  //   ) {
-  //     Swal.fire({
-  //       icon: "error",
-  //       title: "Oops...",
-  //       text: "One or more fields are invalid!",
-  //     });
-  //     return;
-  //   }
-  //   console.log("datos correctos");
-  //   const productoActualizado = {
-  //     ProductName: productoNameRef.current.value,
-  //     Productdetalle: productoEditarRef.current.value,
-  //     PriceProduct: productoPriceRef.current.value,
-  //     ImgURL: productoUrlRef.current.value,
-  //     Category: productoEditar.Category,
-  //     Graduation: productoPorcentajeRef.current.value,
-  //     Avaliable: productoEditar.Avaliable,
-  //   };
-  //   Swal.fire({
-  //     title: "Do you want to update this product?",
-  //     icon: "question",
-  //     showCancelButton: true,
-  //     confirmButtonColor: "#3085d6",
-  //     cancelButtonColor: "#d33",
-  //     confirmButtonText: "Accept",
-  //   }).then(async (result) => {
-  //     if (result.isConfirmed) {
-  //       try {
-  //         const resp = await instance.put(
-  //           `/productos/${id}`,
-  //           productoActualizado
-  //         );
-  //         if (resp.status === 200) {
-  //           Swal.fire(
-  //             "Updated",
-  //             "The product was successfully updated.",
-  //             "success"
-  //           );
-  //           navigate(`/tablaproducto`);
-  //         }
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     }
-  //   });
-  // };
-
+ 
   return (
     <div>
       <Container className="py-5">
