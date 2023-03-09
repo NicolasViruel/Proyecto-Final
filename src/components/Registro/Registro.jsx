cbfedc85c2d31fdeaf87043271eeed5ba0803f1e
nicolas <nicolasviruel@gmail.com>
2023-03-09 18:10:00 -0300

    Se agrega un texto en resgitro para dar el cartel de verificacion de registrado

--- src/components/Registro/Registro.jsx
+++ src/components/Registro/Registro.jsx
@@ -77,6 +77,11 @@ const Registro = ({ show, handleClose }) => {
       };
       try {
         const res = await instance.post("/auth/register", newRegister);
+        Swal.fire({
+          icon: "success",
+          title: "Welcome!",
+          text: "Now you are registered!",
+        });
         Swal.close();
         handleClose();
       } catch (error) {
